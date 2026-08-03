3017a26f2f2bc8b9e43f20c59aba73cb84b77ef6
jeremy0129 <jeremy990129@gmail.com>
2026-08-03 22:59:23 +0800

    DSUtemplate

---
+++ Lesson/DisjointSet/DSUtemplate.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fastio ios::sync_with_stdio(0), cin.tie(0) ,cout.tie(0)
+class DSU{
+    public:
+        vector<int> f ,sz;
+        DSU(int n){
+            f.resize(n+1);
+            sz.resize(n+1,1);
+            for(int i=0;i<n+1;i++){
+                f[i]=i;
+            }
+        }
+        int find(int x){
+                if(f[x]==x) return x;
+                f[x]=find(f[x]);
+                return f[x];
+            }
+        void unite(int x,int y){
+                x=find(x);
+                y=find(y);
+                if(x==y) return;
+                if(sz[y]>sz[x]) swap(x,y);
+                sz[x]+=sz[y];
+                f[y]=x;
+        }
+};
