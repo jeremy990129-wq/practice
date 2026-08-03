0cee4bc1523fd0112774add608ef5f7a073ba7e5
jeremy0129 <jeremy990129@gmail.com>
2026-08-03 23:03:39 +0800

    zerojudge s677

---
+++ Lesson/DisjointSet/s677.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fastio ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+class DSU{
+    public:
+        vector<int> f,sz;
+        DSU(int n){
+            f.resize(n+1);
+            sz.resize(n+1,1);
+            for(int i=0;i<n+1;i++){
+                f[i]=i;
+            }
+        }
+        int find(int x){
+            if(f[x]==x) return x;
+            f[x]=find(f[x]);
+            return f[x];
+        }
+        void unite(int x,int y){
+            x=find(x);
+            y=find(y);
+            if (x==y) return;
+            if(sz[x]<sz[y]) swap(x,y);
+            sz[x]+=sz[y];
+            f[y]=x;
+        }
+
+};
+int main(){
+    fastio;
+    int n,q;
+    cin>>n>>q;
+    DSU dsu(n);
+    
+    while(q--){
+        int b;
+        cin>>b;
+        if(b-1){
+            int u;
+            cin>>u;
+            cout<<dsu.sz[dsu.find(u)]<<'\n';
+        }else{
+            int u,v;
+            cin>>u>>v;
+            dsu.unite(u,v);
+        }
+    }
+}
